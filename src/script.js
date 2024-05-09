4b9b60b72a43b0489365325f98c684a2241222ba
Sidy Diallo <diallosidymohamed99@gmail.com>
2024-05-10 01:26:57 +0200

    fin responsivite des ecrans de 550px et 380px

--- src/script.js
+++ src/script.js
@@ -1,4 +1,59 @@
 
+/*GESTION DE L'ENVOI DU MESSAGE  */
+
+const btn = document.querySelector('.btn');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_z7331eg';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+
+/*FIN DU MESSAGE  */
+
+/*GESTION BURGER */
+
+const icon =document.querySelector(".icon")
+const header =document.querySelector(".header")
+const items =document.getElementById("items")
+const exit = document.querySelector(".exit" )
+
+
+icon.addEventListener("click",()=>{
+    items.classList.remove("nav-bar-items")
+    items.classList.add("popup")
+    icon.style.display="none"
+    exit.style.display="block"
+    
+})
+
+exit.addEventListener("click" ,()=>{
+    items.classList.add("nav-bar-items")
+    items.classList.remove("popup")
+    icon.style.display="block"
+    exit.style.display="none"
+
+
+})
+
+
+/*FIN GESTION BURGER */
+
+/*CLICK SUR LES ITEMS DE LA BARRE DE NAVIGATION  */
 
 const navBarItems = document.querySelectorAll('.nav-bar-items a');
 
@@ -14,6 +69,8 @@ navBarItems.forEach(item => {
     })
     
 });
+
+/*FIN CLICK */
 
 new Typed('.passion', {
     strings : [
